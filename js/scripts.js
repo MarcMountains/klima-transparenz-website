5f2cd4fc9c9e92ceaa630c8a76c45d1e5dff10e2
MarcMountains <marc.berge@iu-study.org>
2024-08-13 22:20:33 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
         nav.style.justifyContent = 'flex-start';
     }
 });
+
+function sortTable(columnIndex) {
+    const table = document.getElementById('emission-table');
+    const rows = Array.from(table.rows).slice(1);
+    const sortedRows = rows.sort((a, b) => {
+        const aText = a.cells[columnIndex].innerText;
+        const bText = b.cells[columnIndex].innerText;
+        return aText.localeCompare(bText, undefined, {numeric: true});
+    });
+
+    for (const row of sortedRows) {
+        table.tBodies[0].appendChild(row);
+    }
+}
